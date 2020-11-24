31ad3631a0dfd8b70f9e6f269c37077a6e9e73c8
Clement Lakhal <clementlakhal@gmail.com>
2020-11-24 12:37:30 +0100

    Update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,7 @@
-import { ethers } from 'hardhat';
+import hre from 'hardhat';
+import '@nomiclabs/hardhat-ethers';
+
+const { ethers } = hre;
 
 async function main() {
   const [deployer] = await ethers.getSigners();
